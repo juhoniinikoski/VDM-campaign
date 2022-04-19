76f4813740f78274d1904b99b889ab537282c5e5
juhoniinikoski <juho.niinikoski@aalto.fi>
2022-04-19 11:53:51 +0300

    fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,9 @@ const Layout: React.FunctionComponent = ({ children }) => {
   return (
     <div className="main-container">
       <div>{children}</div>
-      <div style={{ marginBottom: 100 }}></div>
+      <div style={{ marginTop: 64, height: 70, width: '100%', textAlign: 'center' }}>
+        <p>©2022 #sinunkinpuolestasirokotettu</p>
+      </div>
     </div>
   );
 };
